cd655d995ec081ceef5745b7593acc2d872cbf44
Sean Padden <sean.padden7@gmail.com>
2019-11-06 12:06:57 -0500

    doesnt account for more than two capitals

--- algos.js
+++ algos.js
@@ -8,8 +8,23 @@ function filter_list(array) {
     if(typeof element === "number"){
       result.push(element)
     }
-   })
-   console.log(result)
+  })
+  console.log(result)
+}
+///Run it
+// filter_list([1,2,3, "a", "b"])
+
+// Abbreviate a two word name where there will always be two uppercase letters
+function abbrevName(name){
+  let abbreviation = []
+  for (let i = 0; i < name.length; i++){
+    if (name.charAt(i) == name.charAt(i).toUpperCase()){
+      abbreviation.push(name.charAt(i))
+    }   
+  }
+  abbreviation.splice(1, 0, '.')
+  let result = abbreviation.join('')
+  return result.replace(/ /g,'')
 }
 
-filter_list([1,2,3, "a", "b"])
+abbrevName("Sean Padden")
