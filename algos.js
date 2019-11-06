2c2857b00fe37cb27606dfd0fa013c91f97fbc03
Sean Padden <sean.padden7@gmail.com>
2019-11-06 11:29:03 -0500

    filtering numbers

--- algos.js
+++ algos.js
@@ -1,1 +1,15 @@
 /// Practicing algos!
+
+// Given an array of numbers and strings, print only the numbers
+
+function filter_list(array) {
+  let result = []
+  array.map(element => {
+    if(typeof element === "number"){
+      result.push(element)
+    }
+   })
+   console.log(result)
+}
+
+filter_list([1,2,3, "a", "b"])
