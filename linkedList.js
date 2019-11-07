ef125b3b02d082f950ea230745c64527f011b427
Sean Padden <sean.padden7@gmail.com>
2019-11-07 17:13:04 -0500

    working

---
+++ linkedList.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+var first = new Node("Hi")
+first.next = new Node("there")
+first.next.next = new Node("how")
+first.next.next.next = new Node("are")
+first.next.next.next.next = new Node("you")
+
+
+
+first 
