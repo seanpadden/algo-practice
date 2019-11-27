f3b3b040f92aa737c585805b417406520a492576
Sean Padden <sean.padden7@gmail.com>
2019-11-27 11:47:49 -0500

    add removing nodes problem - come back

---
+++ removeNodes.js
@@ -0,0 +1,10 @@
+/// Given a singly linked list, remove nodes greater than X
+/// return a reference to the root node of the final list
+
+/// PARAMETERS:
+// listHead: reference to the root note of the singly linked list
+// x: integer, the maximum value to be included in the returned singly linked list 
+function removeNodes(listHead, x) {
+  
+
+}
