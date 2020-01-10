4e2affd7c324bacdce45f05ebf90fc22329dfbac
Sean Padden <sean.padden7@gmail.com>
2020-01-10 13:45:57 -0500

    first example

--- currying.js
+++ currying.js
@@ -1,3 +1,10 @@
-function curry(){
-  
+function curryGreeting(a){
+  function hello(b){
+    console.log( 'hi ' + a + ' and ' + b)
+  }
+  return hello
 }
+
+chill = curryGreeting('frank')
+
+chill('dill')
