b4e03798ab58c89c4136943a09375c36b6ab75f1
Sean Padden <sean.padden7@gmail.com>
2020-01-23 12:20:20 -0500

    book movie data

---
+++ booksMoviesData.js
@@ -0,0 +1,52 @@
+let data = [{ 
+  type: "book",
+  title: "Alice in Wonderland", 
+  author: "Lewis Carroll" 
+},{ 
+  type: "book",
+  title: "Gone Baby Gone", 
+  author: "Dennis Lehane" 
+},{ 
+  type: "movie",
+  title: "From Dusk to Dawn", 
+  director: "Robert Rodriguez", 
+  writter: [
+    "Quentin Tarantino",
+    "Robert Rodriguez"
+  ]
+},{ 
+  type: "movie",
+  title: "Pulp Fiction", 
+  director: "Quentin Tarantino", 
+  writter: "Quentin Tarantino" 
+},{ 
+  type: "movie",
+  title: "Once Upon a Time in Hollywood", 
+  director: "Quentin Tarantino", 
+  writter: "Quentin Tarantino" 
+},{ 
+  type: "movie",
+  title: "El Mariachi", 
+  director: "Robert Rodriguez", 
+  writter: "Robert Rodriguez" 
+},{ 
+  type: "movie",
+  title: "Spy Kids", 
+  director: "Robert Rodriguez", 
+  writter: "Robert Rodriguez" 
+},{ 
+  type: "movie",
+  title: "Sin City", 
+  director: "Robert Rodriguez", 
+  writter: "Robert Rodriguez" 
+},{ 
+  type: "movie",
+  title: "Gone Baby Gone", 
+  director: "Ben Affleck",
+  writter: [
+    "Ben Affleck",
+    "Aaron Stockard"
+  ] 
+}]
+
+module.exports = data
